8751f5c2a9b8ceb61928e74abf0d07e959bdb81b
1660667 <marcoreusvietnam11@gmail.com>
2019-05-13 23:20:32 +0700

    update file header

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 // Tín
-const url = process.env.DATABASE_URL || 'postgres://postgres:220515@localhost:5432/GroupStudy'
+//const url = process.env.DATABASE_URL || 'postgres://postgres:220515@localhost:5432/GroupStudy'
 //Trường
-//const url = process.env.DATABASE_URL || 'postgres://postgres:truongap21@localhost:5432/GroupStudy'
+const url = process.env.DATABASE_URL || 'postgres://postgres:truongap21@localhost:5432/GroupStudy'
 //qtruongdeptrai
 //const url = process.env.DATABASE_URL || 'postgres://postgres:123@localhost:5433/GroupStudy'
 const db = new Sequelize(url);
